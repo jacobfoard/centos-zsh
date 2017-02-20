73444915b8ddb5c0cbdbe18ad29ef435e3d05e2f
Jacob Foard <jfoard@redventures.com>
2017-02-20 10:42:02 -0500

    update scirpt
    Signed-off-by: Jacob Foard <jfoard@redventures.com>

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 sudo yum install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-wget 
+rm ~/.zshrc
+wget https://raw.githubusercontent.com/jacobfoard/centos-zsh/master/centos.zshrc -O ~/.zshrc
+source ~/.zsrc
