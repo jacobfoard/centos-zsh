95ff4f69394d21d666ac1920f4fd92d68c611345
Jacob Foard <jfoard@redventures.com>
2017-02-20 10:40:29 -0500

    add files
    Signed-off-by: Jacob Foard <jfoard@redventures.com>

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+wget 
